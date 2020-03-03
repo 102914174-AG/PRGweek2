fcc9c76a58e8ccf18c8f9916f9ec68f6656b1429
ADDISON GALE <102914174@student.swin.edu.au>
2020-03-03 14:08:34 +1100

    added program

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace PRGweek2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 18;
+            int lage = 18;
+
+            if(age>=lage) 
+            {
+                Console.WriteLine("DRINK");
+            }
+            else 
+            {
+                Console.WriteLine("DON'T DRINK");
+            }
+
+            Console.WriteLine("PROGRAM FINISHED!");
+        }
+    }
+}
